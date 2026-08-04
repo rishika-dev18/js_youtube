6e5290c2ac70378a7e8d34715154d9c2c8207709
rishika-dev18 <rishikaanuragi18@gmail.com>
2026-08-04 17:47:38 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+//primitive
+//7 type: string,number,boolean,null,undefined,symbol,BigInt
+
+const score= 100
+const scoreValue= 100.8
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('23')
+const anotherId = Symbol('823')
+
+console.log(id === anotherId);
+
+const bigNumber = 2345677777777777n
+
+const heros = ["shaktiman","doga","dogesh bhai"];
+let myObj = {
+    name: "rishi",
+    age: 18,
+}
+
+console.log(typeof (sideTemp));
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof(anotherId));
+
+
+
+
+
+
+//refrence (non primitive)
+//array,objects,function
