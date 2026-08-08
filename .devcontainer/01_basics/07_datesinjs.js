f56b39734c7eed9241c47b4e4563dcda16a174b7
rishika-dev18 <rishikaanuragi18@gmail.com>
2026-08-08 09:50:25 +0000

    date in js

---
+++ .devcontainer/01_basics/07_datesinjs.js
@@ -0,0 +1,38 @@
+//date
+const myDate = new Date
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026,7,8)
+// let myCreatedDate = new Date(2026,7,2,50)
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
+// `${newDate.getMonth()} and the time is ${newDate.getTime()}`
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+
+
+
+
+
+
+
+
+
