e6fe2093463a076669b6f2011a6666084782c347
rishika-dev18 <rishikaanuragi18@gmail.com>
2026-08-17 13:41:03 +0000

    while do while loop

---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+let myArray = ["fahhh","shaktiman","batman","spider"]
+let arr = 0
+while (arr<myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+let score = 11 // output isliye 11 h kyuki phle condition check hogi badme while
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
