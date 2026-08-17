2a7882174c5ccdc8f7b796ffe6c92b76836a507c
rishika-dev18 <rishikaanuragi18@gmail.com>
2026-08-17 14:19:47 +0000

    for of loop

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,33 @@
+//for of
+// ["","",""]
+// [{},{},{}]
+
+const arr = [2,4,8,4,9]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "hello rishi"
+for (const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+}
+
+const map = new Map()
+map.set('IN','India')  //same key value pair reapet nai hoga
+map.set('USA','United state of america')
+map.set('FR','France')
+map.set('IN','India')
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key,':-',value);
+}
+
+
+const myObject = {
+    game1: 'NFS',
+    game2 : 'FF'
+}
+// for (const [key,value] of object) {
+//     console.log(key,':-',value);   
+// }
